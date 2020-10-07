30d3087a75e4ec44c3efa32823455ea23cb95877
Karim Mahdy <48281050+Karim-mahdy@users.noreply.github.com>
2020-10-07 16:25:34 +0200

    Add project files.

---
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
+
+namespace Spice.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel();
+            IndexVM.MenuItems =await _db.MenuItems.Include(x => x.SubCategory).Include(x => x.SubCategory.Category).ToListAsync();
+            IndexVM.Category = await _db.Category.ToListAsync();
+            IndexVM.Coupon = await _db.Coupon.ToListAsync();
+            return View(IndexVM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
